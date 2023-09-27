686d06559902afb54bc78c735b5229fb7a65d62d
Phuong Khanh <Khanh1802@github.com>
2023-09-27 20:14:44 +0700

    EF Relationships

---
+++ Entities/BasketItem.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Entities
+{
+    [Table("BasketItems")]
+    public class BasketItem
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public int Quantity { get; set; }
+        //navigation properties.
+        public Product Product { get; set; }
+        [ForeignKey("ProductId")]
+        public Guid ProductId { get; set; }
+        public Basket Basket { get; set; }
+        [ForeignKey("BasketId")]
+        public Guid BasketId { get; set; }
+    }
+}
