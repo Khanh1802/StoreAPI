2b717de84644459d94c9f9ce7781409c020893e7
Phuong Khanh <Khanh1802@github.com>
2023-10-06 10:00:54 +0700

    Ad a pagination helper classes

---
+++ RequestHelpers/PaginationParams.cs
@@ -0,0 +1,14 @@
+﻿namespace API.RequestHelpers
+{
+    public class PaginationParams
+    {
+        private const int _maxPageSize = 50; //the number of max result on 1 page
+        public int PageNumber { get; set; } = 1; // default 1 when call the first page
+        private int _pageSize = 5; //the result show on page
+        public int PageSize
+        {
+            get => _pageSize; // return _pageSize
+            set => _pageSize = value > _maxPageSize ? _maxPageSize : value;
+        }
+    }
+}
