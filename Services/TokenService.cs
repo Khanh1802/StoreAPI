f27f4962dbda5025fdee4eb8debf105a53f534ab
Phuong Khanh <Khanh1802@github.com>
2023-10-13 12:45:00 +0700

    Creating a token service

---
+++ Services/TokenService.cs
@@ -0,0 +1,53 @@
+﻿using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace API.Services
+{
+    public class TokenService
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly IConfiguration _configuration;
+
+        public TokenService(UserManager<User> userManager,
+            IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        public async Task<string> GenerateTokenAsync(User user)
+        {
+            //Content PayLoad include (Email,UserName,Role) 
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Email,user.Email),
+                new Claim(ClaimTypes.Name,user.UserName)
+            };
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+            // create signature
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWTSetting:TokenKey"]));
+
+            //create credentials
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+            var tokenOptions = new JwtSecurityToken(
+                issuer: null,
+                audience: null,
+                claims: claims,
+                expires: DateTime.Now.AddDays(7),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+        }
+    }
+}
